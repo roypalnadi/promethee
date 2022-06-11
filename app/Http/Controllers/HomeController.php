6b3af75c32bd3b370281aac75d128a8de9478892
Roy Palnadi <revannaldi45@gmail.com>
2022-06-11 20:28:14 +0700

    Frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Alternatif;
+
 class HomeController extends Controller
 {
     /**
@@ -21,8 +23,53 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $data = Promethee::proses();
+        // $data = Promethee::proses();
 
         return view('home');
+    }
+
+    public function alternatif()
+    {
+        $models = Alternatif::get()
+            ->transform(function ($model, $key) {
+                return [
+                    $key + 1,
+                    $model->nama,
+                    '<nobr>
+                    <a href="https://www.nesabamedia.com" target="_blank">
+                        <button class="btn btn-xs btn-default text-primary mx-1 shadow" title="Edit">
+                            <i class="fa fa-lg fa-fw fa-pen"></i>
+                        </button>
+                    </a>
+                    <button class="btn btn-xs btn-default text-danger mx-1 shadow" title="Delete">
+                        <i class="fa fa-lg fa-fw fa-trash"></i>
+                    </button>
+                    </nobr>',
+                ];
+            });
+
+        $heads = [
+            'No',
+            ['label' => 'Nama', 'width' => 70],
+            'Actions',
+        ];
+
+        $config = [
+            'data' => $models->toArray(),
+            'order' => [[1, 'asc']],
+            'columns' => [null, null, ['orderable' => false]],
+        ];
+
+        return view('alternatif', compact('heads', 'config'));
+    }
+
+    public function kriteria()
+    {
+        return view('kriteria');
+    }
+
+    public function proses()
+    {
+        return view('kriteria');
     }
 }
